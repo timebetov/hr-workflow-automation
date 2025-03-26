5506e2179aa131b3cbe4bd62ee8ecc2476ab97f9
timebetovr <timebetov@gmail.com>
2025-03-26 16:47:33 +0500

    FIX route

--- api-gateway/src/main/java/com/hrworkflow/apigateway/filter/RouteValidator.java
+++ api-gateway/src/main/java/com/hrworkflow/apigateway/filter/RouteValidator.java
@@ -2,6 +2,8 @@ package com.hrworkflow.apigateway.filter;
 
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -9,14 +11,14 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
 
+    private final PathMatcher pathMatcher = new AntPathMatcher();
+
     public static final List<String> whiteList = List.of(
-            "/auth/**",
+            "/identity/api/auth/**",
             "/actuator/**",
             "/eureka"
     );
 
     public Predicate<ServerHttpRequest> isSecured = request ->
-            whiteList.stream().noneMatch(
-                    uri -> request.getURI().getPath().equals(uri)
-            );
+                    whiteList.stream().noneMatch(uri -> pathMatcher.match(uri, request.getURI().getPath()));
 }
